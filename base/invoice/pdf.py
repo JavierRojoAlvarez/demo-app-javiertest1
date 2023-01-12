7706d47e857da0d4581588725e2922af357f6580
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-12 11:21:20 +0000

    Remove useless try-except block

--- base/invoice/pdf.py
+++ base/invoice/pdf.py
@@ -7,13 +7,10 @@ def make_pdf_preview(template_src, context, as_preview=False):
     template = get_template(template_src)
     html = template.render(context)
     result = BytesIO()
-    try:
-        pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), dest=result)
-        pdf_file = result
-        print(type(pdf_file))
-        if as_preview:
-            return pdf_file.getvalue()
-        else:
-            return pdf_file
-    except Exception as exc:
-        print(exc)
+    pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), dest=result)
+    pdf_file = result
+    print(type(pdf_file))
+    if as_preview:
+        return pdf_file.getvalue()
+    else:
+        return pdf_file
