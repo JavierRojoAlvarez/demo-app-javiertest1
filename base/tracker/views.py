64c99508c920369e750f55a6542b28017c03f4e9
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-12 11:00:03 +0000

    Remove useless mixin from tracker

--- base/tracker/views.py
+++ base/tracker/views.py
@@ -7,15 +7,5 @@ class LoginRequiredUrlMixin(LoginRequiredMixin):
     login_url = reverse_lazy('login')
 
 
-class ActiveMixin:
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        active_keys = self.active_keys
-        if active_keys:
-            for key in active_keys:
-                context[key] = 'active'
-        return context
-
-
 class TrackerTemplateView(LoginRequiredUrlMixin, TemplateView):
     template_name = 'tracker/tracker.html'
