d81a31ac5297c3ce2a51f9c3d0eff241beadedb6
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-16 11:38:18 +0000

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'semi': ['error', 'always'],
+    'object-curly-spacing': ['error', 'never'],
+    'space-before-function-paren': 'off',
+    'quote-props': ['error', 'consistent'],
+    'space-infix-ops': 'off',
+    'comma-dangle': ['error', 'only-multiline'],
+  }
+};
