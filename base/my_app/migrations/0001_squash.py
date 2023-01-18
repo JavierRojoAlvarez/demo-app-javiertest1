eab136b50b0e9687b52e2179c164aef7e2f5f3b5
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-18 11:58:36 +0000

    Fix migration by removing old `CreateModel` operations

--- base/my_app/migrations/0001_squash.py
+++ base/my_app/migrations/0001_squash.py
@@ -123,13 +123,6 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='Region',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(default='', max_length=30)),
-            ],
-        ),
-        migrations.CreateModel(
             name='TransactionGroup',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
@@ -207,19 +200,6 @@ class Migration(migrations.Migration):
             model_name='contract',
             name='organisation',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='organisations.organisation', verbose_name='Counterparty'),
-        ),
-        migrations.CreateModel(
-            name='Building',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('epims_id', models.CharField(max_length=50, verbose_name='ePIMS ID')),
-                ('name', models.CharField(max_length=50, verbose_name='Name')),
-                ('nia', models.DecimalField(decimal_places=2, default=0, max_digits=20, verbose_name='NIA')),
-                ('ftes_capacity', models.DecimalField(decimal_places=2, default=0, max_digits=20, verbose_name='FTEs Capacity')),
-                ('image', models.ImageField(default='images/default.png', upload_to='images')),
-                ('cost_centre', models.CharField(default='', max_length=50, verbose_name='Cost Centre')),
-                ('region', models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, to='buildings.region')),
-            ],
         ),
         migrations.AddField(
             model_name='account',
