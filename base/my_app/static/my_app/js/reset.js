b03f67bd1641be831106c39b20427c8fda1f2ce1
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-20 11:15:01 +0000

    Use arrow functions for brevity

--- base/my_app/static/my_app/js/reset.js
+++ base/my_app/static/my_app/js/reset.js
@@ -2,13 +2,9 @@ function reset(fieldId) {
   document.getElementById(fieldId).selectedIndex=0;
 };
 
-document.getElementById('resetBtnRegion').addEventListener('click', function() {
-  reset('region');
-});
+document.getElementById('resetBtnRegion').addEventListener('click', () => reset('region'));
 
-document.getElementById('resetBtnSort').addEventListener('click', function() {
-  reset('sort_by');
-});
+document.getElementById('resetBtnSort').addEventListener('click', () => reset('sort_by'));
 
 document.getElementById('resetBtnAll').addEventListener('click', function() {
   reset('region');
