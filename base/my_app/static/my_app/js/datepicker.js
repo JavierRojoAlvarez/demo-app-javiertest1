30980f8c884c68219db545ab308ebe7a18fd77ad
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-20 11:01:09 +0000

    Lint datepicker.js

--- base/my_app/static/my_app/js/datepicker.js
+++ base/my_app/static/my_app/js/datepicker.js
@@ -1,13 +1,17 @@
+const $ = window.$;
+const getDateLimits = window.getDateLimits;
+
 function addDatePicker() {
-	var dateInputs=$('.date-input');
-	var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-	dateInputs.datepicker({
-			format: 'dd/mm/yyyy',
-			container: container,
-			todayHighlight: true,
-			autoclose: true,
-	}).on('changeDate', getDateLimits);
+  const dateInputs = $('.date-input');
+  const container = $('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : 'body';
+  dateInputs.datepicker({
+    format: 'dd/mm/yyyy',
+    container,
+    todayHighlight: true,
+    autoclose: true,
+  }).on('changeDate', getDateLimits);
 };
+
 $(document).ready(
-	addDatePicker
+  addDatePicker
 );
