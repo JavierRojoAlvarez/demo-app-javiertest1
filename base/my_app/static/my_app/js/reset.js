e4362a5e7ed5d6c473db7e545b09b6e2cfc14df1
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-20 11:04:54 +0000

    Lint reset.js

--- base/my_app/static/my_app/js/reset.js
+++ base/my_app/static/my_app/js/reset.js
@@ -1,14 +1,16 @@
-function reset(fieldId){
-	document.getElementById(fieldId).selectedIndex=0;
+function reset(fieldId) {
+  document.getElementById(fieldId).selectedIndex=0;
 };
 
-document.getElementById("resetBtnRegion").addEventListener("click", function(){
-	reset('region');
+document.getElementById('resetBtnRegion').addEventListener('click', function() {
+  reset('region');
 });
-document.getElementById("resetBtnSort").addEventListener("click", function(){
-	reset('sort_by');
+
+document.getElementById('resetBtnSort').addEventListener('click', function() {
+  reset('sort_by');
 });
-document.getElementById("resetBtnAll").addEventListener("click", function(){
-	reset('region');
-	reset('sort_by');
+
+document.getElementById('resetBtnAll').addEventListener('click', function() {
+  reset('region');
+  reset('sort_by');
 });
