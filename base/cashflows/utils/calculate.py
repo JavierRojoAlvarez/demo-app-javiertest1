91951186c7dc2a6b5a4b4fccb9f893c87cd6ae70
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-26 11:17:34 +0000

    Add whitespace around operators

--- base/cashflows/utils/calculate.py
+++ base/cashflows/utils/calculate.py
@@ -21,12 +21,12 @@ def calculate(
     base_fy_start = 2020
     y = np.arange(0, len(cashflow_df))
     period_start = np.tile(t, len(y))
-    period_end = period_start+np.timedelta64(3, 'M')-np.timedelta64(1, 'D')
-    period_dur = (period_end-period_start+1).astype(int)
+    period_end = period_start + np.timedelta64(3, 'M') - np.timedelta64(1, 'D')
+    period_dur = (period_end - period_start + 1).astype(int)
     year = period_start.astype('datetime64[Y]').astype(int) + 1970
     month = period_start.astype('datetime64[M]').astype(int) % 12 + 1
-    fy_start = np.where(month != 1, year, year-1)
-    time_index = fy_start-base_fy_start
+    fy_start = np.where(month != 1, year, year - 1)
+    time_index = fy_start - base_fy_start
     cross_index = np.repeat(y, len(t))
     start = cashflow_df['start'].values.astype('datetime64[D]')
     end = cashflow_df['end'].values.astype('datetime64[D]')
@@ -34,8 +34,8 @@ def calculate(
     end = np.repeat(end, len(t))
     span = (np.minimum(period_end, end) -
             np.maximum(period_start, start)).astype(int)
-    span = np.where(span >= 0, span+1, span)
-    period_frac = span/period_dur
+    span = np.where(span >= 0, span + 1, span)
+    period_frac = span / period_dur
     lump_sum = np.where(start == end, True, False)
     vector_list = [period_start, period_end, period_dur,
                    fy_start, time_index, cross_index, period_frac, lump_sum]
@@ -49,10 +49,12 @@ def calculate(
     df = df[in_date_condition]
     df = df.merge(cashflow_df, left_on='cross_index', right_index=True)
     df = df.merge(building_df, left_on='building', right_index=True)
-    df['fy_string'] = df['fy_start'].map(str)+"/"+(df['fy_start']+1).map(str)
-    df['value_norm'] = (df['value']/4).where(~df['lump_sum'], df['value'])
+    df['fy_string'] = (
+        df['fy_start'].map(str) + '/' + (df['fy_start'] + 1).map(str)
+    )
+    df['value_norm'] = (df['value'] / 4).where(~df['lump_sum'], df['value'])
     df['value_norm'] = (
-        (df['value_norm']*df['period_frac'])
+        (df['value_norm'] * df['period_frac'])
         .where(~df['lump_sum'], df['value_norm'])
     )
     if groupby:
@@ -62,7 +64,7 @@ def calculate(
     print(f'Rows, Columns = {df.shape}')
     print(f'Datapoints = {df.size}')
     print('Calculation Complete!')
-    duration = datetime.now()-start_time
+    duration = datetime.now() - start_time
     print(f'Duration: {duration}')
 
     return data
