f41c09c608cf9ecadfe4ccdc236aab593d67f3d7
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-01-26 11:36:55 +0000

    Remove useless import statement

--- base/invoices/admin.py
+++ base/invoices/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.apps import apps
-from invoices.models import *
 from import_export.admin import ImportExportModelAdmin
 app_dict = apps.all_models['invoices']
 model_list = [v for v in app_dict.values()]
